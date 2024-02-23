57d15e2be5583bba53b442107fe570790b613b27
sofiaparraweb <sofiaparraweb@gmail.com>
2024-02-23 18:05:43 -0300

    landing

---
+++ restaurant-app/src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Footer</h1>
+      {/* Contenido relacionado con locales */}
+    </div>
+  );
+}
+
+export default Footer;
---
+++ restaurant-app/src/components/navBar/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      {/* Contenido de la barra de navegación */}
+    </nav>
+  );
+}
+
+export default Navbar;
